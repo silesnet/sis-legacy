4d5951fcaf4e34b77b414b3998622d3b887d669e
rsi <rsi@154598ab-59bc-6f4c-bcdc-a89bd969cede>
2009-01-12 06:56:03 +0000

    Incomplete - fix Customer.updated field to be auto set
    git-svn-id: file:///c/home/pal/proj/sis/sis_svn_696/trunk@677 154598ab-59bc-6f4c-bcdc-a89bd969cede

--- java/cz/silesnet/dao/hibernate/CustomerDAOHibernate.java
+++ java/cz/silesnet/dao/hibernate/CustomerDAOHibernate.java
@@ -1,6 +1,6 @@
 package cz.silesnet.dao.hibernate;
 
-import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.apache.commons.logging.Log;
@@ -19,102 +19,101 @@ import cz.silesnet.utils.SearchUtils;
 
 /**
  * Concrete implementation of CustomerDAO using Hibernate.
- *
+ * 
  * @author Richard Sikora
  */
-public class CustomerDAOHibernate
-    extends HibernateDaoSupport
-    implements CustomerDAO {
+public class CustomerDAOHibernate extends HibernateDaoSupport implements CustomerDAO {
 
-    protected final Log    log  = LogFactory.getLog(getClass());
+    protected final Log log = LogFactory.getLog(getClass());
 
-    //~ Methods ----------------------------------------------------------------
+    // ~ Methods
+    // ----------------------------------------------------------------
 
     public List<Customer> getAll() {
-    	DetachedCriteria crit = DetachedCriteria.forClass(Customer.class);
-    	crit.addOrder(SqlHibernateOrder.asc(SearchUtils.getTranslateOrder("name")));
-    	// get only active customers
-    	getHibernateTemplate().enableFilter("activeFilter").setParameter("isActive", true);
-    	return (ArrayList<Customer>) getHibernateTemplate().findByCriteria(crit);
+        DetachedCriteria crit = DetachedCriteria.forClass(Customer.class);
+        crit.addOrder(SqlHibernateOrder.asc(SearchUtils.getTranslateOrder("name")));
+        // get only active customers
+        getHibernateTemplate().enableFilter("activeFilter").setParameter("isActive", true);
+        return getHibernateTemplate().findByCriteria(crit);
     }
 
     public List<Customer> getByExample(Customer cExample) {
-    	if (cExample == null)
-    		return null;
-    	DetachedCriteria crit = DetachedCriteria.forClass(Customer.class);
-    	// general restrictions
-    	SearchUtils.addIlikeRestrictionI18n(crit, "name", cExample.getName());
-//    	SearchUtils.addIlikeRestriction(crit, "public_id", cExample.getPublicId());
-    	// first 8 digits (excluding '/') are export public_id, use this export public id instead of raw public id
-    	SearchUtils.addIlikeRestriction(crit, "substring(replace(public_id, '/', ''), 1, 8)", cExample.getExportPublicId());
-    	SearchUtils.addIlikeInRestriction(crit, "contract_no", cExample.getContractNo());
-    	SearchUtils.addIlikeInRestriction(crit, "symbol", cExample.getSymbol());
-    	// contact restrictions
-    	if (cExample.getContact() != null) {
-    		SearchUtils.addIlikeRestriction(crit, "email", cExample.getContact().getEmail());
-    		// address restrictions
-    		if (cExample.getContact().getAddress() != null) {
-    			Address address = cExample.getContact().getAddress();
-    			SearchUtils.addIlikeRestrictionI18n(crit, "street", address.getStreet());
-    			SearchUtils.addIlikeRestrictionI18n(crit, "city", address.getCity());
-    			SearchUtils.addEqRestriction(crit, "country", address.getCountry());
-    		}
-    	}
-    	// billig restrictions
-    	if (cExample.getBilling() != null) {
-    		Billing billing = cExample.getBilling();
-    		SearchUtils.addEqRestriction(crit, "frequency", billing.getFrequency());
-    		SearchUtils.addEqRestriction(crit, "is_billed_after", billing.getIsBilledAfter());
-    		SearchUtils.addEqRestriction(crit, "is_active", billing.getIsActive());
-    		SearchUtils.addEqRestriction(crit, "status", billing.getStatus());
-    	}
-    	// set custom translate ordering
-    	crit.addOrder(SqlHibernateOrder.asc(SearchUtils.getTranslateOrder("name")));
-    	return (ArrayList<Customer>) getHibernateTemplate().findByCriteria(crit);
+        if (cExample == null)
+            return null;
+        DetachedCriteria crit = DetachedCriteria.forClass(Customer.class);
+        // general restrictions
+        SearchUtils.addIlikeRestrictionI18n(crit, "name", cExample.getName());
+        // SearchUtils.addIlikeRestriction(crit, "public_id",
+        // cExample.getPublicId());
+        // first 8 digits (excluding '/') are export public_id, use this export
+        // public id instead of raw public id
+        SearchUtils.addIlikeRestriction(crit, "substring(replace(public_id, '/', ''), 1, 8)", cExample
+                .getExportPublicId());
+        SearchUtils.addIlikeInRestriction(crit, "contract_no", cExample.getContractNo());
+        SearchUtils.addIlikeInRestriction(crit, "symbol", cExample.getSymbol());
+        // contact restrictions
+        if (cExample.getContact() != null) {
+            SearchUtils.addIlikeRestriction(crit, "email", cExample.getContact().getEmail());
+            // address restrictions
+            if (cExample.getContact().getAddress() != null) {
+                Address address = cExample.getContact().getAddress();
+                SearchUtils.addIlikeRestrictionI18n(crit, "street", address.getStreet());
+                SearchUtils.addIlikeRestrictionI18n(crit, "city", address.getCity());
+                SearchUtils.addEqRestriction(crit, "country", address.getCountry());
+            }
+        }
+        // billig restrictions
+        if (cExample.getBilling() != null) {
+            Billing billing = cExample.getBilling();
+            SearchUtils.addEqRestriction(crit, "frequency", billing.getFrequency());
+            SearchUtils.addEqRestriction(crit, "is_billed_after", billing.getIsBilledAfter());
+            SearchUtils.addEqRestriction(crit, "is_active", billing.getIsActive());
+            SearchUtils.addEqRestriction(crit, "status", billing.getStatus());
+        }
+        // set custom translate ordering
+        crit.addOrder(SqlHibernateOrder.asc(SearchUtils.getTranslateOrder("name")));
+        return getHibernateTemplate().findByCriteria(crit);
     }
-    
+
     public void evict(Customer customer) {
         getHibernateTemplate().evict(customer);
     }
 
     public Customer get(Long customerId) {
-        Customer customer = (Customer) getHibernateTemplate()
-                                               .get(Customer.class, customerId);
+        Customer customer = (Customer) getHibernateTemplate().get(Customer.class, customerId);
 
         if (customer == null)
             throw new ObjectRetrievalFailureException(Customer.class, customerId);
 
         return customer;
     }
-    
-	public Customer load(Long customerId) {
-        Customer customer = (Customer) getHibernateTemplate()
-        										.load(Customer.class, customerId);
-		if (customer == null)
-			throw new ObjectRetrievalFailureException(Customer.class, customerId);
-		return customer;
-	}
 
+    public Customer load(Long customerId) {
+        Customer customer = (Customer) getHibernateTemplate().load(Customer.class, customerId);
+        if (customer == null)
+            throw new ObjectRetrievalFailureException(Customer.class, customerId);
+        return customer;
+    }
 
     public void remove(Customer customer) {
         getHibernateTemplate().delete(customer);
     }
 
     public void save(Customer customer) {
+        customer.setUpdated(new Date());
         getHibernateTemplate().saveOrUpdate(customer);
     }
 
-	public void saveAll(List<Customer> customers) {
-		getHibernateTemplate().saveOrUpdateAll(customers);
-	}
-	
-	public int getTotalCustomers(Country c) {
-    	getHibernateTemplate().enableFilter("activeFilter").setParameter("isActive", true);
-		Integer total = c == null
-			? (Integer) getHibernateTemplate().find("select count(*) from Customer").get(0)
-			: (Integer) getHibernateTemplate().find("select count(*) from Customer c" +
-					" where c.contact.address.country=?", c).get(0);
-		return total != null ? total : 0;
-	}
+    public void saveAll(List<Customer> customers) {
+        getHibernateTemplate().saveOrUpdateAll(customers);
+    }
+
+    public int getTotalCustomers(Country c) {
+        getHibernateTemplate().enableFilter("activeFilter").setParameter("isActive", true);
+        Integer total = c == null ? (Integer) getHibernateTemplate().find("select count(*) from Customer").get(0)
+                : (Integer) getHibernateTemplate().find(
+                        "select count(*) from Customer c" + " where c.contact.address.country=?", c).get(0);
+        return total != null ? total : 0;
+    }
 
 }
