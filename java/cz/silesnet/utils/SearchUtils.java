9abc12880035f64d86c14231c07893410d55eeba
rsi <rsi@154598ab-59bc-6f4c-bcdc-a89bd969cede>
2009-02-04 22:49:05 +0000

    fixed PL customers edit bug caused by not enclosing strings in apostrophes
    git-svn-id: file:///c/home/pal/proj/sis/sis_svn_696/trunk@687 154598ab-59bc-6f4c-bcdc-a89bd969cede

--- java/cz/silesnet/utils/SearchUtils.java
+++ java/cz/silesnet/utils/SearchUtils.java
@@ -15,67 +15,75 @@ import cz.silesnet.model.enums.EnumPersistenceMapping;
 
 public class SearchUtils {
 
-//	private static String sFromChars =	"e��������ͣ����؊�?���ݎ?����������������?����.-,;:&+ ";
-//	private static String sFromChars =	"�?�???�??��???��?�??�?�ݎ??�?�???�??��???��?�??�?��??.-,;:&+ ";
-	private static String sFromChars =	"ÁĄÄČĆĎÉĚĘËÍŁŇŃÓÖŘŠŚŤÚŮÜÝŽŻŹáąäčćďéěęëíłňńóöřšśťúůüýžżź.-,;:&+ ";
-	private static String sToChars = 	"aaaccdeeeeilnnoorsstuuuyzzzaaaccdeeeeilnnoorsstuuuyzzz";
-	
-	public static String getFromChars() {
-		return sFromChars;
-	}
+    // private static String sFromChars =
+    // "e��������ͣ����؊�?���ݎ?����������������?����.-,;:&+ ";
+    // private static String sFromChars =
+    // "�?�???�??��???��?�??�?�ݎ??�?�???�??��???��?�??�?��??.-,;:&+ ";
+    private static String sFromChars = "ÁĄÄČĆĎÉĚĘËÍŁŇŃÓÖŘŠŚŤÚŮÜÝŽŻŹáąäčćďéěęëíłňńóöřšśťúůüýžżź.-,;:&+ ";
+    private static String sToChars = "aaaccdeeeeilnnoorsstuuuyzzzaaaccdeeeeilnnoorsstuuuyzzz";
 
-	public static String getToChars() {
-		return sToChars;
-	}
-	
-	public static String getTranslateColumn(String arg) {
-		return "lower(translate(" + arg + ",'" + getFromChars() + "','" + getToChars() + "'))";
-	}
+    public static String getFromChars() {
+        return sFromChars;
+    }
 
-	public static String translate(String s) {
-		return StringUtils.replaceChars(s, getFromChars(), getToChars()).toLowerCase();
-	}
-	
-	public static String getTranslateOrder(String col) {
-		return "translate(" + col + ",'" + getFromChars() + "','" + getToChars() + "')";
-	}
-	
+    public static String getToChars() {
+        return sToChars;
+    }
+
+    public static String getTranslateColumn(String arg) {
+        return "lower(translate(" + arg + ",'" + getFromChars() + "','" + getToChars() + "'))";
+    }
+
+    public static String translate(String s) {
+        return StringUtils.replaceChars(s, getFromChars(), getToChars()).toLowerCase();
+    }
+
+    public static String getTranslateOrder(String col) {
+        return "translate(" + col + ",'" + getFromChars() + "','" + getToChars() + "')";
+    }
+
     public static void addIlikeRestriction(DetachedCriteria crit, String col, String value) {
-    	if (value != null)
-    		crit.add(Restrictions.sqlRestriction(col + " ilike('" + value + "%')"));
+        if (value != null)
+            crit.add(Restrictions.sqlRestriction(col + " ilike('" + value + "%')"));
     }
 
     public static void addIlikeInRestriction(DetachedCriteria crit, String col, String value) {
-    	if (value != null)
-    		crit.add(Restrictions.sqlRestriction(col + " ilike('%" + value + "%')"));
+        if (value != null)
+            crit.add(Restrictions.sqlRestriction(col + " ilike('%" + value + "%')"));
     }
 
     public static void addIlikeRestrictionI18n(DetachedCriteria crit, String col, String value) {
-    	if (value != null)
-    		addIlikeRestriction(crit, getTranslateColumn(col), translate(value));
+        if (value != null)
+            addIlikeRestriction(crit, getTranslateColumn(col), translate(value));
     }
-    
+
     public static void addIlikeInRestrictionI18n(DetachedCriteria crit, String col, String value) {
-    	if (value != null)
-    		addIlikeInRestriction(crit, getTranslateColumn(col), translate(value));
+        if (value != null)
+            addIlikeInRestriction(crit, getTranslateColumn(col), translate(value));
     }
 
     public static void addEqRestriction(DetachedCriteria crit, String col, Object value) {
-		if (value != null) {
-			crit.add(Restrictions.sqlRestriction(col + "=" + value));
-		}
-	}
+        if (value != null) {
+            crit.add(Restrictions.sqlRestriction(col + "=" + value));
+        }
+    }
+
+    public static void addEqRestriction(DetachedCriteria crit, String col, String value) {
+        if (value != null) {
+            crit.add(Restrictions.sqlRestriction(col + "='" + value + "'"));
+        }
+    }
 
     public static void addEqRestriction(DetachedCriteria crit, String col, EnumPersistenceMapping<?> enumerator) {
-		if (enumerator != null) {
-			crit.add(Restrictions.sqlRestriction(col + "=" + enumerator.getId()));
-		}
-	}
-    
+        if (enumerator != null) {
+            crit.add(Restrictions.sqlRestriction(col + "=" + enumerator.getId()));
+        }
+    }
+
     public static void addEqRestriction(DetachedCriteria crit, String col, Entity entity) {
-		if (entity != null) {
-			crit.add(Restrictions.sqlRestriction(col + "=" + entity.getId()));
-		}
-	}
+        if (entity != null) {
+            crit.add(Restrictions.sqlRestriction(col + "=" + entity.getId()));
+        }
+    }
 
 }
