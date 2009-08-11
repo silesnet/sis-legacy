1b526fa80c4ee42db2117f7e63e0d5b00cfeb4c5
der3k <rs.pipe@gmail.com>
2009-08-12 00:05:02 +0200

    added tests

---
+++ src/test/java/cz/silesnet/dao/hibernate/HistoryItemDAOHibernateTest.java
@@ -0,0 +1,18 @@
+package cz.silesnet.dao.hibernate;
+
+import junit.framework.TestCase;
+
+public class HistoryItemDAOHibernateTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testRemoveLoginHistoryOlderThan() throws Exception {
+		fail("Not yet implemented.");
+	}
+}
---
+++ src/test/java/cz/silesnet/service/impl/HistoryManagerImplTest.java
@@ -0,0 +1,19 @@
+package cz.silesnet.service.impl;
+
+import junit.framework.TestCase;
+
+public class HistoryManagerImplTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testRemoveLoginHistoryOlderThan() {
+		fail("Not yet implemented");
+	}
+
+}
