000b7db17f43f6af33d89bc0062cd897d6be39e7
der3k <rs.pipe@gmail.com>
2009-08-27 11:54:18 +0200

    #18 implemented -month to date calculations

--- src/test/java/cz/silesnet/service/impl/HistoryManagerImplTest.java
+++ src/test/java/cz/silesnet/service/impl/HistoryManagerImplTest.java
@@ -1,9 +1,15 @@
 package cz.silesnet.service.impl;
 
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Matchers.*;
 import static org.mockito.Mockito.*;
 import junit.framework.TestCase;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.joda.time.DateTime;
+import org.joda.time.Period;
 import org.mockito.ArgumentCaptor;
 
 import cz.silesnet.dao.HistoryItemDAO;
@@ -11,29 +17,52 @@ import cz.silesnet.service.HistoryManager;
 
 public class HistoryManagerImplTest extends TestCase {
 
-	private HistoryManagerImpl hm;
+    private static final int ITEMS_REMOVED = 2;
 
-	private HistoryItemDAO dao;
+    private static final int MONTHS = 10;
 
-	protected void setUp() throws Exception {
-		super.setUp();
-		hm = new HistoryManagerImpl();
-		dao = mock(HistoryItemDAO.class);
-		hm.setHistoryItemDAO(dao);
-	}
+    protected final Log log = LogFactory.getLog(getClass());
 
-	protected void tearDown() throws Exception {
-		super.tearDown();
-		hm = null;
-		dao = null;
-	}
+    private HistoryManagerImpl hm;
 
-	public void testRemoveMonthsOldLoginHistory() {
-		ArgumentCaptor<DateTime> date = ArgumentCaptor.forClass(DateTime.class);
-		when(dao.removeLoginHistoryOlderThan(date.capture())).thenReturn(2);
-		hm
-				.removeMonthsOldLoginHistory(HistoryManager.MIN_MONTHS_LOGIN_HISTORY_AGE);
-		// TODO test the value
-		System.out.println(date.getValue());
-	}
+    private HistoryItemDAO dao;
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        hm = new HistoryManagerImpl();
+        dao = mock(HistoryItemDAO.class);
+        hm.setHistoryItemDAO(dao);
+    }
+
+    protected void tearDown() throws Exception {
+        super.tearDown();
+        hm = null;
+        dao = null;
+    }
+
+    public void testRemoveMonthsOldLoginHistory() {
+        when(dao.removeLoginHistoryOlderThan(isA(DateTime.class))).thenReturn(
+                ITEMS_REMOVED);
+        assertThat(hm.removeMonthsOldLoginHistory(MONTHS), is(ITEMS_REMOVED));
+    }
+
+    public void testRemoveMonthsOldLoginHistoryDate() {
+        hm.removeMonthsOldLoginHistory(MONTHS);
+        ArgumentCaptor<DateTime> removeFrom = ArgumentCaptor
+                .forClass(DateTime.class);
+        verify(dao).removeLoginHistoryOlderThan(removeFrom.capture());
+        Period period = new Period(removeFrom.getValue(), new DateTime());
+        assertThat(period.getMonths(), is(MONTHS));
+    }
+
+    public void testRemoveMonthsOldLoginHistoryIllegalMonths() {
+        try {
+            hm
+                    .removeMonthsOldLoginHistory(HistoryManager.MIN_MONTHS_LOGIN_HISTORY_AGE - 1);
+            fail();
+        } catch (AssertionError e) {
+            // expected
+        }
+    }
+
 }
