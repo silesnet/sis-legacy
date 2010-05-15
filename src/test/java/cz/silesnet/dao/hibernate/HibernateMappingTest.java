dab843885cf22578c871adbaa30385760830ea48
der3k <rs.pipe@gmail.com>
2010-05-15 17:11:23 +0200

    hibernate mapping by unitils implemented

---
+++ src/test/java/cz/silesnet/dao/hibernate/HibernateMappingTest.java
@@ -0,0 +1,21 @@
+package cz.silesnet.dao.hibernate;
+
+import org.testng.annotations.Test;
+import org.unitils.UnitilsTestNG;
+import org.unitils.orm.hibernate.HibernateUnitils;
+import org.unitils.spring.annotation.SpringApplicationContext;
+
+/**
+ * User: der3k
+ * Date: May 15, 2010
+ * Time: 11:31:20 AM
+ */
+@SpringApplicationContext({"context/sis-properties.xml", "context/sis-db.xml", "context/sis-hibernate.xml"})
+public class HibernateMappingTest extends UnitilsTestNG {
+
+    @Test
+    public void hibernateMappings() throws Exception {
+        HibernateUnitils.assertMappingWithDatabaseConsistent();
+    }
+}
+
