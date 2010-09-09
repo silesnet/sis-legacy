350201c2b6a07be9e7d0dd4399e59a9da0e653a1
der3k <rs.pipe@gmail.com>
2010-09-09 23:44:12 +0200

    upgraded to spring 2.5.6.SEC02

--- src/test/java/cz/silesnet/service/impl/CustomerControllerTest.java
+++ src/test/java/cz/silesnet/service/impl/CustomerControllerTest.java
@@ -22,60 +22,60 @@ import static org.testng.Assert.*;
 @Test(groups = "integration")
 public class CustomerControllerTest {
 
-    protected final Log log = LogFactory.getLog(getClass());
+  protected final Log log = LogFactory.getLog(getClass());
 
-    @Test
-    public void testSecurity() {
-        String[] paths = {"context/sis-application.xml"};
-        ApplicationContext ctx = new ClassPathXmlApplicationContext(paths);
-        assertNotNull(ctx);
+  @Test
+  public void testSecurity() {
+    String[] paths = {"context/sis-application.xml"};
+    ApplicationContext ctx = new ClassPathXmlApplicationContext(paths);
+    assertNotNull(ctx);
 
-        SecurityContext sc = SecurityContextHolder.getContext();
-        assertNotNull(sc);
-        assertNull(sc.getAuthentication());
+    SecurityContext sc = SecurityContextHolder.getContext();
+    assertNotNull(sc);
+    assertNull(sc.getAuthentication());
 
-        // set authentication in context
-        GrantedAuthority roleTest = new GrantedAuthorityImpl("ROLE_FAKE");
-        GrantedAuthority[] authorities = new GrantedAuthority[]{roleTest};
-        Authentication user = new AnonymousAuthenticationToken("anonymousKey",
-                "anonymousUser", authorities);
-        assertTrue(user.isAuthenticated());
-        sc.setAuthentication(user);
+    // set authentication in context
+    GrantedAuthority roleTest = new GrantedAuthorityImpl("ROLE_FAKE");
+    GrantedAuthority[] authorities = new GrantedAuthority[]{roleTest};
+    Authentication user = new AnonymousAuthenticationToken("anonymousKey",
+        "anonymousUser", authorities);
+    assertTrue(user.isAuthenticated());
+    sc.setAuthentication(user);
 
-        // we are prepared for calling secured method on container managed bean
-        Object controller = ctx.getBean("customerController");
-        assertNotNull(controller);
+    // we are prepared for calling secured method on container managed bean
+    Object controller = ctx.getBean("customerController");
+    assertNotNull(controller);
 
-        // use reflection to execute test method
-        Method[] methods = ReflectionUtils.getAllDeclaredMethods(controller
-                .getClass());
-        assertNotNull(methods);
-        log.debug(controller.getClass().getName());
-        Method test = null;
-        for (Method m : methods) {
-            log.debug(m.getName());
-            if ("showForm".equals(m.getName()))
-                test = m;
-        }
-
-        log.debug(test);
-        try {
-            test.invoke(controller, new Object[]{null, null});
-            fail();
-        }
-        catch (InvocationTargetException e) {
-            if (e.getCause() instanceof AccessDeniedException)
-                log.debug("Caught expected exception: " + e.getCause());
-            else {
-                log.debug(e.getCause());
-                // FIXME implement the test correctly
-//				fail();
-            }
-        }
-        catch (Throwable e) {
-            log.debug(e);
-            fail();
-        }
+    // use reflection to execute test method
+    Method[] methods = ReflectionUtils.getAllDeclaredMethods(controller
+        .getClass());
+    assertNotNull(methods);
+    log.debug(controller.getClass().getName());
+    Method test = null;
+    for (Method m : methods) {
+      log.debug(m.getName());
+      if ("showForm".equals(m.getName()))
+        test = m;
     }
+
+    log.debug(test);
+    try {
+      test.invoke(controller, new Object[]{null, null});
+      fail();
+    }
+    catch (InvocationTargetException e) {
+      if (e.getCause() instanceof AccessDeniedException)
+        log.debug("Caught expected exception: " + e.getCause());
+      else {
+        log.debug(e.getCause());
+        // FIXME implement the test correctly
+//				fail();
+      }
+    }
+    catch (Throwable e) {
+      log.debug(e);
+      fail();
+    }
+  }
 
 }
