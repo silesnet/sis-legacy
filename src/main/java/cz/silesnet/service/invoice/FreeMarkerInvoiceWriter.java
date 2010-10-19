5eadfacc02d938c81c65da98cb53774cba1debd9
der3k <rs.pipe@gmail.com>
2010-10-19 21:00:56 +0200

    adding free marker support for InvoiceWriter

--- src/main/java/cz/silesnet/service/invoice/FreeMarkerInvoiceWriter.java
+++ src/main/java/cz/silesnet/service/invoice/FreeMarkerInvoiceWriter.java
@@ -1,16 +1,39 @@
 package cz.silesnet.service.invoice;
 
 import cz.silesnet.model.Bill;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * User: der3k
  * Date: 19.10.2010
  * Time: 20:36:24
  */
-public abstract class FreeMarkerInvoiceWriter extends AbstractInvoiceWriter {
+public class FreeMarkerInvoiceWriter extends AbstractInvoiceWriter {
 
-  protected FreeMarkerInvoiceWriter(Bill bill) {
+  protected Template template;
+  private static final String INVOICE_MODEL_NAME = "invoice";
+
+  protected FreeMarkerInvoiceWriter(Bill bill, Template template) {
     super(bill);
+    this.template = template;
   }
-  
+
+  @Override
+  public void writeTo(final PrintWriter writer) {
+    Map<String, Object> model = new HashMap<String, Object>();
+    model.put(INVOICE_MODEL_NAME, bill);
+    try {
+      template.process(model, writer);
+    } catch (TemplateException e) {
+      throw new RuntimeException(e);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
 }
--- src/main/java/cz/silesnet/service/invoice/InvoiceWriterFactory.java
+++ src/main/java/cz/silesnet/service/invoice/InvoiceWriterFactory.java
@@ -7,19 +7,6 @@ import cz.silesnet.model.Bill;
  *
  * @author Richard Sikora
  */
-public class InvoiceWriterFactory {
-
-  private InvoiceWriterFactory() {
-  }
-
-  public static InvoiceWriter instanceOf(Bill invoice, InvoiceFormat format) {
-    if (InvoiceFormat.LINK.equals(format))
-      return new LinkInvoiceWriter(invoice);
-    if (InvoiceFormat.HTML.equals(format))
-      return new HtmlInvoiceWriter(invoice);
-    if (InvoiceFormat.PDF.equals(format))
-      return new PdfInvoiceWriter(invoice);
-    return null;
-  }
-
+public interface InvoiceWriterFactory {
+  InvoiceWriter instanceOf(Bill invoice, InvoiceFormat format);
 }
