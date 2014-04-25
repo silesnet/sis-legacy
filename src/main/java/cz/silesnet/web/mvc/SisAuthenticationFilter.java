6c35b9cfb6050640974d8d0bf6db07531b1ddccd
der3k <sikora@silesnet.cz>
2014-04-25 09:41:14 +0200

    added proper SisAuthenticationFilter tests

--- src/main/java/cz/silesnet/web/mvc/SisAuthenticationFilter.java
+++ src/main/java/cz/silesnet/web/mvc/SisAuthenticationFilter.java
@@ -8,11 +8,14 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationEventPublisherAware;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
-import org.springframework.security.core.Authentication;
+import org.springframework.security.authentication.event.InteractiveAuthenticationSuccessEvent;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.util.Assert;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.GenericFilterBean;
@@ -25,6 +28,7 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
@@ -39,130 +43,189 @@ import java.util.List;
  * Time: 17:47
  * To change this template use File | Settings | File Templates.
  */
-public class SisAuthenticationFilter extends GenericFilterBean {
-    private final Log log = LogFactory.getLog(getClass());
-    private Client client;
-    private ObjectMapper mapper;
+public class SisAuthenticationFilter extends GenericFilterBean implements ApplicationEventPublisherAware {
+	private final Log log = LogFactory.getLog(getClass());
+	private Client client;
+	private ObjectMapper mapper;
 
-    private String userUrl;
-    private String loginUrl;
-    private String sessionId = "JSESSIONID";
+	private String userUrl;
+	private String loginUrl;
+	private String sessionId = "JSESSIONID";
 
-    public void setUserUrl(String userUrl) {
-        this.userUrl = userUrl;
-    }
+	private ApplicationEventPublisher applicationEventPublisher;
 
-    public void setLoginUrl(String loginUrl) {
-        this.loginUrl = loginUrl;
-    }
+	public void setUserUrl(String userUrl) {
+		this.userUrl = userUrl;
+	}
 
-    public void setSessionId(String sessionId) {
-        this.sessionId = sessionId;
-    }
+	public void setLoginUrl(String loginUrl) {
+		this.loginUrl = loginUrl;
+	}
 
-		protected void setClient(Client client) {
-			this.client = client;
-		}
+	public void setSessionId(String sessionId) {
+		this.sessionId = sessionId;
+	}
 
-		protected void setMapper(ObjectMapper mapper) {
-			this.mapper = mapper;
-		}
+	protected void setClient(Client client) {
+		this.client = client;
+	}
+
+	protected void setMapper(ObjectMapper mapper) {
+		this.mapper = mapper;
+	}
+
+	public void setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
+		this.applicationEventPublisher = applicationEventPublisher;
+	}
 
 	@Override
-    public void afterPropertiesSet() throws ServletException {
-        super.afterPropertiesSet();
+	public void afterPropertiesSet() throws ServletException {
+		super.afterPropertiesSet();
 
-        Assert.hasLength(userUrl);
-        Assert.hasLength(loginUrl);
-        Assert.hasLength(sessionId);
+		Assert.hasLength(userUrl);
+		Assert.hasLength(loginUrl);
+		Assert.hasLength(sessionId);
 
-        TrustManager[] trustAllCerts = new TrustManager[] {
-            new X509TrustManager() {
-                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-                    return null;
-                }
+		TrustManager[] trustAllCerts = new TrustManager[]{
+				new X509TrustManager() {
+					public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+						return null;
+					}
 
-                public void checkClientTrusted(X509Certificate[] certs, String authType) {
+					public void checkClientTrusted(X509Certificate[] certs, String authType) {
 
-                }
+					}
 
-                public void checkServerTrusted(X509Certificate[] certs, String authType) {
+					public void checkServerTrusted(X509Certificate[] certs, String authType) {
 
-                }
-            }
-        };
+					}
+				}
+		};
 
-        SSLContext sslContext;
-        try {
-            sslContext = SSLContext.getInstance("SSL");
-            sslContext.init(null, trustAllCerts, new SecureRandom());
-        } catch (NoSuchAlgorithmException e) {
-            throw new ServletException(e);
-        } catch (KeyManagementException e) {
-            throw new ServletException(e);
-        }
+		SSLContext sslContext;
+		try {
+			sslContext = SSLContext.getInstance("SSL");
+			sslContext.init(null, trustAllCerts, new SecureRandom());
+		} catch (NoSuchAlgorithmException e) {
+			throw new ServletException(e);
+		} catch (KeyManagementException e) {
+			throw new ServletException(e);
+		}
 
-        HostnameVerifier allHostsValid = new HostnameVerifier()
-        {
-            public boolean verify(String hostname, SSLSession session)
-            {
-                return true;
-            }
-        };
+		HostnameVerifier allHostsValid = new HostnameVerifier() {
+			public boolean verify(String hostname, SSLSession session) {
+				return true;
+			}
+		};
 
-        final DefaultClientConfig config = new DefaultClientConfig();
-        config.getProperties().put(HTTPSProperties.PROPERTY_HTTPS_PROPERTIES, new HTTPSProperties(allHostsValid, sslContext));
-        client = Client.create(config);
+		final DefaultClientConfig config = new DefaultClientConfig();
+		config.getProperties().put(HTTPSProperties.PROPERTY_HTTPS_PROPERTIES, new HTTPSProperties(allHostsValid, sslContext));
+		client = Client.create(config);
+		mapper = new ObjectMapper();
+	}
 
-        mapper = new ObjectMapper();
-    }
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		final HttpServletRequest req = (HttpServletRequest) request;
+		final HttpServletResponse res = (HttpServletResponse) response;
+		final AbstractAuthenticationToken authentication = authentication(req);
+		if (authentication != null) {
+			AbstractAuthenticationToken sessionAuthentication = sessionAuthentication(req);
+			if (sessionAuthentication != null) {
+				if (authentication.getPrincipal().equals(sessionAuthentication.getPrincipal())) {
+					SecurityContextHolder.getContext().setAuthentication(authentication);
+				} else {
+					dropSessionIfPresent(req);
+					res.sendRedirect(loginUrl);
+					return;
+				}
+			} else {
+				dropSessionIfPresent(req);
+				final HttpSession session = req.getSession(true);
+				authentication.setDetails(new WebAuthenticationDetails(req));
+				session.setAttribute("authentication", authentication);
+				SecurityContextHolder.getContext().setAuthentication(authentication);
+				applicationEventPublisher.publishEvent(new InteractiveAuthenticationSuccessEvent(authentication, SisAuthenticationFilter.class));
+			}
+			chain.doFilter(request, response);
+		} else {
+			dropSessionIfPresent(req);
+			res.sendRedirect(loginUrl);
+		}
+	}
 
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-      final HttpServletRequest req = (HttpServletRequest) request;
-      final HttpServletResponse res = (HttpServletResponse) response;
-	    if (req.getSession(false) == null) {
-		    req.getSession(true);
-	    }
-	    final Cookie[] cookies = req.getCookies();
-        String userData = null;
-        if (cookies != null) {
-            for (int i = cookies.length; 0 < i; i--) {
-                final Cookie cookie = cookies[i - 1];
-                if (sessionId.equals(cookie.getName())) {
-                    final String url = userUrl + ";jsessionid=" + cookie.getValue();
-                    log.debug(url);
-                    final WebResource resource = client.resource(url);
-                    userData = resource.get(String.class);
-                    log.debug(userData);
-                    break;
-                }
-            }
-        }
-        if (StringUtils.hasLength(userData)) {
-            final JsonNode user = mapper.readTree(userData);
-            final String principal = user.get("user").getValueAsText();
-            final List<GrantedAuthority> authorities = AuthorityUtils.commaSeparatedStringToAuthorityList(user.get("roles").getValueAsText());
+	protected AbstractAuthenticationToken authentication(HttpServletRequest request) {
+		final String user = fetchUserJsonStringFromRemoteService(request);
+		final AbstractAuthenticationToken authentication = authenticationFromJson(user);
+		return authentication;
+	}
 
-            Authentication authentication = new AbstractAuthenticationToken(authorities) {
+	protected String fetchUserJsonStringFromRemoteService(HttpServletRequest request) {
+		final Cookie[] cookies = request.getCookies();
+		String userData = null;
+		if (cookies != null) {
+			for (int i = cookies.length; 0 < i; i--) {
+				final Cookie cookie = cookies[i - 1];
+				if (sessionId.equals(cookie.getName())) {
+					final String url = userUrl + ";jsessionid=" + cookie.getValue();
+					log.debug(url);
+					final WebResource resource = client.resource(url);
+					userData = resource.get(String.class);
+					log.debug(userData);
+					break;
+				}
+			}
+		}
+		return userData;
+	}
 
-                public Object getCredentials() {
-                    return null;
-                }
+	protected AbstractAuthenticationToken authenticationFromJson(String json) {
+		AbstractAuthenticationToken authentication = null;
+		if (StringUtils.hasLength(json)) {
+			final JsonNode node;
+			try {
+				node = mapper.readTree(json);
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+			final JsonNode user = node.get("user");
+			final JsonNode roles = node.get("roles");
+			if (user != null && roles != null) {
+				final String principal = user.getValueAsText();
+				final List<GrantedAuthority> authorities = AuthorityUtils.commaSeparatedStringToAuthorityList(roles.getValueAsText());
+				authentication = new AbstractAuthenticationToken(authorities) {
 
-                public Object getPrincipal() {
-                    return principal;
-                }
+					public Object getCredentials() {
+						return null;
+					}
 
-                @Override
-                public boolean isAuthenticated() {
-                    return true;
-                }
-            };
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-	          req.getSession(false).setAttribute("authentication", authentication);
-            chain.doFilter(request, response);
-        } else {
-            res.sendRedirect(loginUrl);
-        }
-    }
+					public Object getPrincipal() {
+						return principal;
+					}
+
+					@Override
+					public boolean isAuthenticated() {
+						return true;
+					}
+				};
+			}
+		}
+		return authentication;
+	}
+
+	private AbstractAuthenticationToken sessionAuthentication(HttpServletRequest request) {
+		AbstractAuthenticationToken authentication = null;
+		final HttpSession session = request.getSession(false);
+		if (session != null) {
+			authentication = (AbstractAuthenticationToken) session.getAttribute("authentication");
+		}
+		return authentication;
+	}
+
+	private void dropSessionIfPresent(HttpServletRequest req) {
+		final HttpSession session = req.getSession(false);
+		if (session != null) {
+			session.invalidate();
+		}
+	}
+
 }
--- src/test/java/cz/silesnet/web/mvc/SisAuthenticationFilterTest.java
+++ src/test/java/cz/silesnet/web/mvc/SisAuthenticationFilterTest.java
@@ -4,13 +4,19 @@ import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.WebResource;
 import cz.silesnet.util.SecurityUtils;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.mockito.ArgumentCaptor;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.testng.annotations.Test;
 
 import javax.servlet.FilterChain;
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpSession;
 
 import static org.mockito.Mockito.*;
 import static org.testng.Assert.*;
@@ -18,7 +24,110 @@ import static org.testng.Assert.*;
 public class SisAuthenticationFilterTest {
 
 	@Test
-	public void sessionCreationWhenLogged() throws Exception {
+	public void notAuthenticatedWithNoSession() throws Exception {
+		SisAuthenticationFilter filter = filterWithAuthResponse("{}");
+		MockHttpServletRequest req = requestWithCookies();
+		MockHttpServletResponse res = new MockHttpServletResponse();
+		FilterChain chain = mock(FilterChain.class);
+
+		assertNull(req.getSession(false), "no session before filter call");
+		assertNull(res.getRedirectedUrl(), "no redirect before filter call");
+
+		filter.doFilter(req, res, chain);
+
+		assertNull(req.getSession(false), "no session after filter call");
+		assertEquals(res.getRedirectedUrl(), "http://localhost:8080/login.jsp", "redirected to login url");
+		verify(chain, never()).doFilter(req, res);
+	}
+
+	@Test
+	public void notAuthenticatedWithExpiredSession() throws Exception {
+		SisAuthenticationFilter filter = filterWithAuthResponse("{}");
+		MockHttpServletRequest req = requestWithCookies();
+		MockHttpServletResponse res = new MockHttpServletResponse();
+		FilterChain chain = mock(FilterChain.class);
+
+		assertNotNull(req.getSession(true), "session before filter call");
+		assertNull(res.getRedirectedUrl(), "no redirect before filter call");
+
+		filter.doFilter(req, res, chain);
+
+		assertNull(req.getSession(false), "no session after filter call");
+		assertEquals(res.getRedirectedUrl(), "http://localhost:8080/login.jsp", "redirected to login url");
+		verify(chain, never()).doFilter(req, res);
+	}
+
+	@Test
+	public void authenticatedWithNoSession() throws Exception {
+		SisAuthenticationFilter filter = filterWithAuthResponse("{ \"user\" : \"rsi\", \"roles\": \"ROLE_ANONYMOUS\" }");
+		MockHttpServletRequest req = requestWithCookies();
+		MockHttpServletResponse res = new MockHttpServletResponse();
+		ApplicationEventPublisher publisher = mock(ApplicationEventPublisher.class);
+		filter.setApplicationEventPublisher(publisher);
+		FilterChain chain = mock(FilterChain.class);
+
+		assertNull(req.getSession(false), "no session before filter call");
+
+		filter.doFilter(req, res, chain);
+
+		final HttpSession session = req.getSession(false);
+		assertNotNull(session, "session after filter call");
+		final Object attribute = session.getAttribute("authentication");
+		assertNotNull(attribute, "authentication in session");
+		final AbstractAuthenticationToken authentication = (AbstractAuthenticationToken) attribute;
+		assertEquals(authentication.getPrincipal(), "rsi", "principal in authentication");
+		assertEquals(authentication.getDetails().getClass(), WebAuthenticationDetails.class, "details in authentication");
+		assertEquals(SecurityUtils.currentUser(), "rsi", "security context is populated");
+		final ArgumentCaptor<ApplicationEvent> captor = ArgumentCaptor.forClass(ApplicationEvent.class);
+		verify(publisher).publishEvent(captor.capture());
+		final ApplicationEvent event = captor.getValue();
+		assertNotNull(event, "event was published");
+		assertEquals(event.getSource(), authentication, "event contains authentication");
+		verify(chain).doFilter(req, res);
+	}
+
+	@Test
+	public void authenticatedWithValidSession() throws Exception {
+		SisAuthenticationFilter filter = filterWithAuthResponse("{ \"user\" : \"rsi\", \"roles\": \"ROLE_ANONYMOUS\" }");
+		MockHttpServletRequest req = requestWithCookies();
+		MockHttpServletResponse res = new MockHttpServletResponse();
+		FilterChain chain = mock(FilterChain.class);
+
+		final AbstractAuthenticationToken authentication = filter.authentication(req);
+		assertNotNull(authentication, "authentication exists");
+
+		final HttpSession session = req.getSession(true);
+		assertNotNull(session, "session before filter call");
+		session.setAttribute("authentication", authentication);
+
+		filter.doFilter(req, res, chain);
+
+		assertEquals(SecurityUtils.currentUser(), "rsi", "security context is populated");
+		verify(chain).doFilter(req, res);
+	}
+
+	@Test
+	public void authenticatedWithMismatchedSession() throws Exception {
+		SisAuthenticationFilter filter = filterWithAuthResponse("{ \"user\" : \"rsi\", \"roles\": \"ROLE_ANONYMOUS\" }");
+		MockHttpServletRequest req = requestWithCookies();
+		MockHttpServletResponse res = new MockHttpServletResponse();
+		FilterChain chain = mock(FilterChain.class);
+
+		final AbstractAuthenticationToken sessionAuthentication = new UsernamePasswordAuthenticationToken("a", "b");
+		assertNotNull(sessionAuthentication, "authentication exists");
+
+		final HttpSession session = req.getSession(true);
+		assertNotNull(session, "session before filter call");
+		session.setAttribute("authentication", sessionAuthentication);
+
+		filter.doFilter(req, res, chain);
+
+		assertNull(req.getSession(false), "no session after filter call");
+		assertEquals(res.getRedirectedUrl(), "http://localhost:8080/login.jsp", "redirected to login url");
+		verify(chain, never()).doFilter(req, res);
+	}
+
+	private SisAuthenticationFilter filterWithAuthResponse(String response) {
 		SisAuthenticationFilter filter = new SisAuthenticationFilter();
 		filter.setLoginUrl("http://localhost:8080/login.jsp");
 		filter.setUserUrl("http://localhost:8080/get_user");
@@ -26,20 +135,15 @@ public class SisAuthenticationFilterTest {
 		Client client = mock(Client.class);
 		filter.setClient(client);
 		WebResource resource = mock(WebResource.class);
-		when(resource.get(String.class)).thenReturn("{ \"user\" : \"rsi\", \"roles\": \"ROLE_ANONYMOUS\" }");
+		when(resource.get(String.class)).thenReturn(response);
 		when(client.resource("http://localhost:8080/get_user;jsessionid=12345")).thenReturn(resource);
+		return filter;
+	}
 
+	private MockHttpServletRequest requestWithCookies() {
 		MockHttpServletRequest req = new MockHttpServletRequest();
 		req.setCookies(cookies());
-		MockHttpServletResponse res = new MockHttpServletResponse();
-		FilterChain chain = mock(FilterChain.class);
-		assertNull(req.getSession(false), "no session before filter call");
-		filter.doFilter(req, res, chain);
-
-		assertEquals(SecurityUtils.currentUser(), "rsi", "user has been populated to security context");
-		assertNotNull(req.getSession(false), "session has been created");
-		String principal = (String) ((AbstractAuthenticationToken) req.getSession(false).getAttribute("authentication")).getPrincipal();
-		assertEquals(principal, "rsi");
+		return req;
 	}
 
 	private Cookie[] cookies() {
@@ -47,5 +151,7 @@ public class SisAuthenticationFilterTest {
 		cookies[0] = new Cookie("JSESSIONID", "12345");
 		return cookies;
 	}
+
+
 
 }
